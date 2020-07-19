b1b0876a89a3d20f0eb9eee30f92ef12c6b714e3
Krzysztof Noga <>
2020-07-19 14:27:14 +0200

    deploy

---
+++ geoportal24/chunks/maps-control.js
@@ -0,0 +1,19 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{64:function(t,n,e){"use strict";e.r(n),e.d(n,"MapsControl",(function(){return c}));e(58),e(63),e(62),e(60);var o=e(61),i=e(44),a=e(3);class c{constructor(t){this.options=t}onAdd(t){return this._map=t,this._container=document.createElement("div"),this._container.className="mapboxgl-ctrl",Object(a.c)(i.c`
+                <vaadin-context-menu
+                    selector="vaadin-button"
+                    open-on="click"
+                    .renderer=${n=>{if(!n.firstElementChild){const e=document.createElement("div");Object(a.c)(s({map:t,...this.options}),e),n.appendChild(e)}}}
+                >
+                    <vaadin-button theme="icon">
+                        <iron-icon icon="maps:layers"></iron-icon>
+                    </vaadin-button>
+                </vaadin-context-menu>
+            `,this._container),this._container}onRemove(){this._container.parentNode.removeChild(this._container),this._map=void 0}}const s=Object(o.b)(t=>{const{map:n,basemaps:e,basemap:a}=t,[c,s]=Object(o.a)(a);return i.c`
+        ${e.map(t=>i.c`
+                <vaadin-button
+                    theme="${c===t.id?"primary":"secondary"}"
+                    @click=${e=>{e.stopPropagation(),n.setStyle(t.style),s(t.id)}}
+                    >${t.label}</vaadin-button
+                >
+            `)}
+    `})}}]);
