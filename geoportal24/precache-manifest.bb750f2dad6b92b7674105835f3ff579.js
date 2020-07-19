4257941abbf1ae8f3779535ba44cf674ecd1ec1e
Krzysztof Noga <>
2020-07-19 10:57:27 +0200

    deploy

---
+++ geoportal24/precache-manifest.bb750f2dad6b92b7674105835f3ff579.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "79106fd9b808b3bbd70d54b934850301",
+    "url": "vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "fb2608da8f301832531117b05f4acdb3",
+    "url": "vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "abd77169b68f1b61bc7aa0ef6a9c7a1e",
+    "url": "mapbox-gl/mapbox-gl.css"
+  },
+  {
+    "revision": "e2f0b54cb1564b7467ce56bab4945e77",
+    "url": "vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "d0c0741d9f0612fcaf6589ba17a78bd8",
+    "url": "vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "894a294495257c3d389efa3e1bd9bde7",
+    "url": "vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "3a28d79ed7dae933323bcabb6ad3dade",
+    "url": "vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "69fa52c9b1ebe0d28af7d84323fb887e",
+    "url": "mapbox-gl/mapbox-gl-csp-worker.js"
+  },
+  {
+    "revision": "e6aeb1b486d39700c37772a9ecc10b10",
+    "url": "mapbox-gl/style-spec/index.es.js"
+  },
+  {
+    "revision": "bbb7989c0c0ca64d29384b5379e1c5aa",
+    "url": "mapbox-gl/style-spec/index.js"
+  },
+  {
+    "revision": "0941146e8b8ae007f2ca67ebd8035777",
+    "url": "mapbox-gl/mapbox-gl-csp.js"
+  },
+  {
+    "revision": "c788fca67ea728be90f67bd7ade94141",
+    "url": "mapbox-gl/mapbox-gl.js"
+  },
+  {
+    "revision": "32638f467ef656905fee251a1ea1f07b",
+    "url": "mapbox-gl/mapbox-gl-unminified.js"
+  },
+  {
+    "revision": "dd7fb9eb93497a7f26b45f1ffaad9a5c",
+    "url": "mapbox-gl/mapbox-gl-dev.js"
+  }
+];
--- geoportal24/sw.js
+++ geoportal24/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.3448fb06fdbe2d3b11deec5a0debec18.js", "workbox-v3.6.3/workbox-sw.js");
+importScripts("precache-manifest.bb750f2dad6b92b7674105835f3ff579.js", "workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 if ('workbox' in self) {
   workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
